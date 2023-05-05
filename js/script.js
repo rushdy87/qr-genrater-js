2a7fbf7b22ba9f40d74ef4ec3f2a5a6b99370ff6
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-05-05 10:13:47 +0300

    show and hide spinner

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('Hello, from script file');
+const form = document.querySelector('#generate-form');
+const qr = document.querySelector('#qrcode');
+const spinner = document.querySelector('#spinner');
+
+hideSpinner();
+
+form.addEventListener('submit', onGenerateSubmit);
+
+function onGenerateSubmit(event) {
+  event.preventDefault();
+
+  const url = document.querySelector('#url').value;
+  const size = document.querySelector('#size').value;
+
+  showSpinner();
+  setTimeout(() => hideSpinner(), 1000);
+}
+
+function showSpinner() {
+  spinner.style.display = 'block';
+}
+function hideSpinner() {
+  spinner.style.display = 'none';
+}
